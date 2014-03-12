ed0c8d1ba7cc31eaeb9aa0f2bc8384ed0874763a
Catt Small <cattsmall@gmail.com>
2014-03-12 16:50:58 -0400

    Video page mostly complete.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 $( "#reaction-chart-toggle" ).click(function() {
   $( "#reaction-chart" ).slideToggle( "fast" );
+  if ($(this).find("span").text() == "Show reaction chart") {
+    $(this).find("span").text("Hide reaction chart");
+    $(this).find(".ss-icon").text("directup");
+  }
+  
+  else {
+    $(this).find("span").text("Show reaction chart");
+    $(this).find(".ss-icon").text("dropdown");
+  }
 });
 
 $('.carousel').carousel();
